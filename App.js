cd4e53314f4914674536148410b6d7c5ae399c19
goki sakaguchi <goki0524@gmail.com>
2019-05-04 23:47:10 +0900

    login view

--- App.js
+++ App.js
@@ -1,11 +1,52 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+import { Input, SocialIcon } from 'react-native-elements'
+import Icon from 'react-native-vector-icons/FontAwesome'
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <Input
+          placeholder='    ログインID'
+          leftIcon={
+            <Icon
+              name='user'
+              size={24}
+              color='black'
+            />
+          }
+        />
+        <Input
+          placeholder='   パスワード'
+          containerStyle={{marginTop: 20}}
+          leftIcon={
+            <Icon
+              name='key'
+              size={24}
+              color='black'
+            />
+          }
+        />
+        <SocialIcon
+          title='Sign In Oaks'
+          button
+          type='medium'
+          style={{marginTop: 40}}
+        />
+
+        <View style={styles.socialIcon}>
+          <SocialIcon
+            title='Sign In With Facebook'
+            button
+            type='facebook'
+          />
+          <SocialIcon
+            title='Sign In With Twitter'
+            button
+            type='twitter'
+          />
+        </View>
       </View>
     );
   }
@@ -15,7 +56,9 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
   },
+  socialIcon: {
+    marginTop: 80,
+  }
 });
